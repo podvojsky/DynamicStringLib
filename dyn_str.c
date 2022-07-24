4d66eb8663161246ee06ab553fcd88d8e375ba40
Lukas Podvojsky <65615145+daitykz@users.noreply.github.com>
2022-07-24 17:55:14 +0200

    Init function definition

--- dyn_str.c
+++ dyn_str.c
@@ -1,1 +1,20 @@
 #include "dyn_str.h"
+
+extern int errno;
+
+/**
+ * @brief Initialize dynamic string.
+ * 
+ * @param s String for initialization.
+ */
+void DynStrInit(DynamicString *s) {
+    int init_size = ALLOC_BLOCK_SIZE * sizeof(char);    // Initial size (in bytes) of string to allocate.
+
+    s->value = (char*) malloc(init_size);
+    if (s->value == NULL) {
+        perror("Inside DynStrInit()");
+    }
+    s->size = init_size;
+    s->len = 0;
+    strcpy(s->value, "\0");
+}
