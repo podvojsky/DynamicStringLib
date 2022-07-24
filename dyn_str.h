0b882ce16d0a1e270112c90917fbe3e82113d817
Lukas Podvojsky <65615145+daitykz@users.noreply.github.com>
2022-07-24 17:27:49 +0200

    Added dyn_str.h Defined structure for dynamic string.

---
+++ dyn_str.h
@@ -0,0 +1,17 @@
+#ifndef DYN_STR_H
+#define DYN_STR_H
+
+#define ALLOC_BLOCK_SIZE 1024
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+typedef struct {
+    char *value;    // Stored string value (with null terminator)
+    int len;        // String lenght (number of characters excluding null character)
+    int size;       // String size that has been alocated
+} DynamicString;
+
+#endif
